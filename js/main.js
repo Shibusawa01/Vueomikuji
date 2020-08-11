c333ddcee1353c09ceffe3aa18b8a224863dc0e5
Shibusawa01 <68539805+Shibusawa01@users.noreply.github.com>
2020-08-11 12:48:03 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+new Vue({
+  el: "#title",
+  data: {
+    title: "今夜の夕食を決めよう！",
+    push: "ボタンを押してね",
+    menu: "Menu",
+    btn: "なにが出るかな？",
+  },
+  methods:{
+    onclick: function(){
+      this.number = Math.floor(Math.random() * 5);
+      
+      curry = "カレーライス";
+      sushi = "寿司";
+      spa = "スパゲッティ";
+      humb = "ハンバーガー";
+      ramen = "ラーメン";
+      
+      switch(this.number){
+        case 0:
+          this.btn = curry;
+          break;
+        case 1:
+          this.btn = sushi;
+          break;
+        case 2:
+          this.btn = spa;
+          break;
+        case 3:
+          this.btn = humb;
+          break;
+        case 4:
+          this.btn = ramen;
+          break;
+      }
+    }
+  }
+})
